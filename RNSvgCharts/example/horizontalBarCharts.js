5ac8fd13e409d4c0922ea07805eb4360f1c98029
congsuboya <liuchaofeichen@163.com>
2017-11-19 21:24:21 +0800

    Track 2 files into repository.
    - modified RNSvgCharts/example/horizontalBarCharts.js
    - modified RNSvgCharts/example/horizontalBarStackCharts.js
    
    Auto commit by GitBook Editor

--- RNSvgCharts/example/horizontalBarCharts.js
+++ RNSvgCharts/example/horizontalBarCharts.js
@@ -83,7 +83,7 @@ HorizontalBarCharts.defaultProps = {
                 name: '直接访问',
                 type: 'bar',
                 barWidth: '60%',
-                data: [10, 5, 2, 3, 10, 7, 6, 5, 2, 30,]
+                data: [10, 5, 2, 3, 10, 7, 6, 5, 2, 3,]
             },
             {
                 name: '非直接访问',
